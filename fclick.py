75d6ead71be6c6686af0e42f66b1488d1bbb34a4
donguyenha <donguyenha@gmail.com>
2017-10-12 18:05:02 +0700

    add flask cli

---
+++ fclick.py
@@ -0,0 +1,30 @@
+from flask_app import app, db
+from flask_app.models.youtube import YOUTUBE
+from flask_app.models.packtpub import PACKTPUB
+from flask_app.models.vtv import VTV
+import sqlalchemy_utils
+from flask_migrate import Migrate
+import click
+
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, VTV=VTV, YOUTUBE=YOUTUBE, PACKTPUB=PACKTPUB)
+
+
+@app.cli.command(with_appcontext=True)
+def create_admin():
+    """Creates the admin user."""
+    click.echo(db)
+    # db.session.add(User(
+    #     email="ad@min.com",
+    #     password="admin",
+    #     username="admin"
+    # )
+    # db.session.commit()
+
+
+# if __name__ == '__main__':
+#     create_admin()
